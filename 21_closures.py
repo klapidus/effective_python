536624d61e6f1f382a55b6a01a4588df211f987e
Kirill Lapidus <kirill.lapidus@gmail.com>
2023-04-01 15:54:43 +0200

    Item 21: Closures and scope

--- 21_closures.py
+++ 21_closures.py
@@ -13,3 +13,25 @@ print('numbers', numbers)
 print('group', group)
 res = sort_priority(numbers, group)
 print('sorted', res)
+
+
+# this is a way to work with a state
+class SortPriority:
+    def __init__(self, group):
+        self.group = group
+        self.found = False
+
+    def __call__(self, x):
+        if x in group:
+            self.found = True
+            return 0, x
+        return 1, x
+
+
+print('numbers', numbers)
+print('group', group)
+sorter = SortPriority(group)
+# note that sorter is a callable, very nice!
+res = sorted(numbers, key=sorter)
+print('sorted', res)
+print('found', sorter.found)
