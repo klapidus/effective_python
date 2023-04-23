500008258d01597f01234b89856c3f12b11a7f8b
Kirill Lapidus <kirill.lapidus@gmail.com>
2023-04-23 17:25:19 +0200

    Item 43: collections.abc

--- 43_collections_abc.py
+++ 43_collections_abc.py
@@ -1,4 +1,4 @@
-from collections.abc import Sequence
+from collections.abc import Sequence, Set
 
 
 # must realize __len__ and __getitem__
@@ -24,13 +24,46 @@ class CustomList(Sequence):
 cl = CustomList([2, 3, 3, 7, 108, 2])
 print(cl[3], cl[3])
 print(cl.counts())
+
 # realized by Sequence
 print(cl.index(108))
 print(cl.index(3))
+print(cl.count(3))
+print(cl.count(200))
+
 # reversed returns a generator
-print(reversed(cl))
+print(reversed(cl))  # is a generator object
 for k in reversed(cl):
     print(k)
 print(list(reversed(cl)))
-print(cl.count(3))
-print(cl.count(200))
+
+
+# note, however, that such operations as union, etc
+# are not realized here
+class SetTwice(Set):
+    """Allows element to appear (only) twice in a set."""
+    def __init__(self, xs: list):
+        self._d = []
+        counts = {}
+        for x in xs:
+            c = counts.get(x, 0) + 1
+            if c <= 2:
+                self._d.append(x)
+            counts[x] = c
+
+    def __contains__(self, item):
+        return item in self._d
+
+    def __iter__(self):
+        yield from self._d
+
+    def __len__(self):
+        return len(self._d)
+
+
+se = SetTwice([1, 2, 2, 3, 3, 3])
+for el in se:
+    print(el)
+
+print("check and operation")
+se2 = SetTwice([1, 3, 3, 3, 4, 4])
